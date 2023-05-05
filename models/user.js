dd6f4f6304e405bf9c524decb1fb5a4f77f39cd1
Salman Ansari <imsalman315@gmail.com>
2023-05-05 19:56:41 +0530

    Minor fix

--- models/user.js
+++ models/user.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
+    name: { type: String },
     avatar: { type: String },
+    bgImg: { type: String },
+    bio: { type: String },
     email: { type: String, required: true },
     password: { type: String, required: true },
     followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
