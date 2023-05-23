52f06239928e0d9459814052c8a28903cb7c62c9
Salman Ansari <imsalman315@gmail.com>
2023-05-24 01:38:18 +0530

    Update env file

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/api', userRoutes)
 app.use('/api/tweets', tweetRoutes)
 
 // SERVER LISTENING
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>
   console.log(`Server running on PORT: http://localhost:${PORT}`)
 )
