edbfcfb78baa92d818dfb796cce21c3eedf12646
Salman Ansari <78205495+salman7875@users.noreply.github.com>
2023-05-24 10:13:06 +0530

    Refactor code

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ const tweetRoutes = require('./routes/tweets')
 const app = express()
 
 // DB CONNECTION
-mongoose
-  .connect(process.env.DB)
-  .then(() => console.log('DB Connected'))
-  .catch(err => console.log(err.message))
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB)
+    console.log('MongoDB Connected: ' + conn.connection.host)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+}
 
 // MIDDLEWARE
 app.use(cors())
@@ -25,6 +30,8 @@ app.use('/api/tweets', tweetRoutes)
 
 // SERVER LISTENING
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () =>
-  console.log(`Server running on PORT: http://localhost:${PORT}`)
-)
+connectDB().then(() => {
+  app.listen(PORT, () =>
+    console.log(`Server running on PORT: http://localhost:${PORT}`)
+  )
+})
