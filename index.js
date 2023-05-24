ed0164886cb9528f2af762d8bce542b06ec1b2cc
Salman Ansari <imsalman315@gmail.com>
2023-05-24 11:42:17 +0530

    Fix

--- index.js
+++ index.js
@@ -9,20 +9,21 @@ const tweetRoutes = require('./routes/tweets')
 const app = express()
 
 // DB CONNECTION
-// mongoose
-//   .connect(process.env.DB)
-//   .then(() => console.log('DB Connected'))
-//   .catch(err => console.log(err.message))
-mongoose.set('strictQuery', false)
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.DB)
-    console.log('MongoDB Connected: ' + conn.connection.host)
-  } catch (err) {
-    console.log(err)
-    process.exit(1)
-  }
-}
+// mongoose.set('strictQuery', false)
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.DB)
+//     console.log('MongoDB Connected: ' + conn.connection.host)
+
+//   } catch (err) {
+//     console.log(err)
+//     process.exit(1)
+//   }
+// }
+
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true })
+  .then(() => console.log('DB Connected'))
 
 // MIDDLEWARE
 app.use(cors())
@@ -35,8 +36,6 @@ app.use('/api/tweets', tweetRoutes)
 
 // SERVER LISTENING
 const PORT = process.env.PORT || 5000
-connectDB().then(() => {
-  app.listen(PORT, () =>
-    console.log(`Server running on PORT: http://localhost:${PORT}`)
-  )
-})
+app.listen(PORT, () =>
+  console.log(`Server running on PORT: http://localhost:${PORT}`)
+)
