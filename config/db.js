9d6a4af6b244782f5854835dd3f53636603109ed
imsajid12 <imsajid12@gmail.com>
2023-09-24 13:27:32 +0530

    db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.DB_PROD, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },)
-        console.log('DB Connected');
-    } catch (err) {
-        console.log(err.message)
-    }
-}
+  try {
+    await mongoose.connect(process.env.DB_PROD, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("DB Connected");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
-module.exports = { connect }
+module.exports = { connect };
